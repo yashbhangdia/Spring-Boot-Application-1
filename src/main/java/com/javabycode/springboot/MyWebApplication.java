91a898b54a34527d52de34abf0151374ed1f0734
yashbhangdia <55742719+yashbhangdia@users.noreply.github.com>
2021-07-08 12:59:03 +0530

    Update MyWebApplication.java

--- src/main/java/com/javabycode/springboot/MyWebApplication.java
+++ src/main/java/com/javabycode/springboot/MyWebApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
 
 @SpringBootApplication
 public class MyWebApplication extends SpringBootServletInitializer{
@@ -15,7 +23,17 @@ public class MyWebApplication extends SpringBootServletInitializer{
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(InetAddress.getLocalHost().getHostAddress());
+//         System.out.println(InetAddress.getLocalHost().getHostAddress());
+        ArrayList<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+        for (NetworkInterface iface : interfaces)
+        {
+            for (InterfaceAddress addr : iface.getInterfaceAddresses()) 
+            {
+                System.out.println("\t\t" + addr.getAddress().toString());
+            } 
+        }
+        
+        
         SpringApplication.run(MyWebApplication.class, args);
     }
 }
